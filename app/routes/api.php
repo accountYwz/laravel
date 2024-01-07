15e07594440e332d52c4012178dc929e6f0965f4
yangXiaoMing <49814620+accountYwz@users.noreply.github.com>
2024-01-07 17:25:05 +0800

    init code

--- app/routes/api.php
+++ app/routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/index', function (Request $request) {
+    return 1000;
+});
